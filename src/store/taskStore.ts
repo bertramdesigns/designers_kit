9fd06486caafc3ec7c02098eab405970ba0189c7
Dylan Bertram <dylan@bertramdesigns.com>
2025-01-23 10:23:49 +0100

    aligned types to server config

--- src/store/taskStore.ts
+++ src/store/taskStore.ts
@@ -1,9 +1,33 @@
 import { createStore } from "solid-js/store";
 import { ID, Role, Permission } from "appwrite";
 import { authState } from "./authStore";
+import { load } from '@tauri-apps/plugin-store';
 
-const [taskStore, setTaskStore] = createStore<TaskState>({
+const [taskStore, setTaskStore] = createStore<TaskStore>({
     tasks: [],
+    lastUpdated: new Date(),
+    loading: false,
+    localStore: null,
+});
+const workspace = "workspace1";
+
+const initLocalStore = async () => {
+    try {
+        // TODO: filename should mirror the workspace
+        const localStore = await load(`${workspace}/store.json`, { autoSave: false });
+        setTaskStore("localStore", localStore);
+        const localTasks: Promise<Task | undefined> = localStore.get("tasks");
+        return localTasks;
+    } catch (error) {
+        console.log(error);
+        throw new Error("Failed to load local store");
+    }
+}
+
+initLocalStore().then((localTasks) => {
+    if (localTasks) {
+        setTaskStore("tasks", localTasks);
+    }
 });
 
 const fetchTasks = async () => {
@@ -17,56 +41,63 @@ const fetchTasks = async () => {
 
     promise.then((response) => {
         tasks = response.documents.map((doc: any) => ({
-            taskid: doc.$id,
+            $id: doc.$id,
+            taskid: doc.taskid,
             title: doc.title,
+            description: doc.description,
             status: doc.completed,
             labels: doc.labels,
             priority: doc.priority,
             startDate: doc.startDate,
             dueDate: doc.dueDate,
             assignedTo: doc.assignedTo,
+            createdBy: doc.createdBy,
+            deleted: doc.deleted,
+            deletedAt: doc.deletedAt,
+            deletedOnPlatform: doc.deletedOnPlatform,
         }));
         setTaskStore("tasks", tasks);
     }, (error) => {
         console.log(error);
     });
-    // const tasks = response.documents.map((doc: any) => ({
-    //     id: doc.$id,
-    //     title: doc.title,
-    //     status: doc.completed,
-    //     label: doc.label,
-    //     priority: doc.priority,
-    // }));
 };
 
 const addTask = async (task: Task) => {
-    // check if authState.user is null. Unauthorized user should not be able to add tasks
-    if (!authState.user) {
+    if (taskStore.localStore === null) {
+        console.error("Local store not initialized");
         return;
     }
 
-    const response = await authState.databases.createDocument(
-        import.meta.env.VITE_APPWRITE_DATABASE_ID,
-        import.meta.env.VITE_APPWRITE_WORKSPACE_1_TASKS_COLLECTION_ID,
-        ID.unique(),
-        task,
-        [
-            Permission.read(Role.user(authState.user.$id)),    // Only this user can read
-            Permission.update(Role.user(authState.user.$id)),  // Only this user can update
-            Permission.delete(Role.user(authState.user.$id))   // Only this user can delete
-        ]
-    );
-    const newTask: Task = {
-        taskid: response.taskid,
-        title: response.title,
-        status: response.completed,
-        labels: response.labels,
-        priority: response.priority,
-        startDate: response.startDate,
-        dueDate: response.dueDate,
-        assignedTo: response.assignedTo,
-    };
-    setTaskStore("tasks", (tasks) => [...tasks, newTask]);
+    // handle logic for unique ID
+    // TODO: handle offline mode & sync
+    if (!task.$id) {
+        task.$id = ID.unique();
+    }
+
+    // set locally first
+    try {
+        await taskStore.localStore.set("tasks", [...taskStore.tasks, task]);
+    } catch (error) {
+        console.log(error);
+    }
+
+    // check if authState.user is null. Unauthorized user should not be able to add tasks to db
+    if (authState.isAuthenticated) {
+        // need to handle id error
+        // const response = await authState.databases.createDocument(
+        await authState.databases.createDocument(
+            import.meta.env.VITE_APPWRITE_DATABASE_ID,
+            import.meta.env.VITE_APPWRITE_WORKSPACE_1_TASKS_COLLECTION_ID,
+            task.$id,
+            task,
+            [
+                Permission.read(Role.user(authState.user.$id)),    // Only this user can read
+                Permission.update(Role.user(authState.user.$id)),  // Only this user can update
+                Permission.delete(Role.user(authState.user.$id))   // Only this user can delete
+            ]
+        );
+    }
+    setTaskStore("tasks", (tasks) => [...tasks, task]);
 };
 
 const updateTask = async (id: string, completed: boolean) => {
@@ -77,14 +108,20 @@ const updateTask = async (id: string, completed: boolean) => {
         completed,
     });
     const updatedTask: Task = {
+        $id: response.$id,
         taskid: response.taskid,
         title: response.title,
+        description: response.description,
         status: response.completed,
         labels: response.labels,
         priority: response.priority,
         startDate: response.startDate,
         dueDate: response.dueDate,
         assignedTo: response.assignedTo,
+        createdBy: response.createdBy,
+        deleted: response.deleted,
+        deletedAt: response.deletedAt,
+        deletedOnPlatform: response.deletedOnPlatform
 
     };
     setTaskStore("tasks", (tasks) => tasks.map((task) => (task.taskid === id ? updatedTask : task)));
--- src/types/tasks.d.ts
+++ src/types/tasks.d.ts
@@ -1,23 +1,48 @@
+import { Store } from "@tauri-apps/plugin-store";
 import type { ID } from "appwrite";
 
 declare global {
-    type TaskStatus = 'open' | 'in-progress' | 'completed';
     type TaskPriority = 'low' | 'medium' | 'high';
 
     type Task = {
+        $id?: string; // Document ID
         taskid: string;
         title?: string;
         description?: string;
-        status: Status;
+        status: string; // TaskStatus;
         labels?: Array<string>;
-        priority?: Priority;
+        priority?: TaskPriority;
         startDate?: Date; // ISO 8601 date string
         dueDate?: Date; // ISO 8601 date string
         assignedTo: Array<string>; // User ID
+        createdBy: string; // User ID
+        deleted?: boolean; // Soft delete for server sync
+        deletedAt?: Date;
+        deletedOnPlatform?: "web" | "mobile" | "desktop";
+        // history: Array<TaskHistory>; // Linked (relationship) to History
     }
 
-    interface TaskState {
+    /* TODO: Implement TaskHistory
+    type TaskHistory = {
+        taskid: string; // Task ID
+        title?: string;
+        description?: string;
+        status: TaskStatus;
+        labels?: Array<string>;
+        priority?: TaskPriority;
+        startDate?: Date; // ISO 8601 date string
+        dueDate?: Date; // ISO 8601 date string
+        assignedTo: Array<string>; // User ID
+        $updatedBy: string; // User ID
+        $updatedDate: Date; // ISO 8601 date string
+    } 
+    */
+
+    interface TaskStore {
         tasks: Task[];
+        lastUpdated: Date;
+        loading: boolean;
+        localStore: Store | null; // instance to store local data
     }
 }
 export { }
