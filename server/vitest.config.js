eb4a1ab9934d13b81fb81e4ddc24e10fa1273ecc
Dylan Bertram <dylan@bertramdesigns.com>
2025-02-16 17:08:11 +0100

    increased hook timout

--- server/vitest.config.js
+++ server/vitest.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         globals: true,
-        hookTimeout: 30_000,
-        testTimeout: 30_000,
+        hookTimeout: 120_000,
+        testTimeout: 60_000,
         environment: 'node',
         setupFiles: './tests/setup.js', // Optional: if you have any setup files
     },
